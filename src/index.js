8dceacb8d7685780e24eca4c53b030dadbbc3dbd
ves-sev <newsecretword@yandex.ru>
2022-11-06 14:04:36 +0300

    redux started in the project

--- src/index.js
+++ src/index.js
@@ -3,11 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app/App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
+const defaultState = {
+  cash: 15
+}
+const reducer = (state = defaultState, action)=>{
+  switch (action.type) {
+    case "ADD_CASH":return {...state,cash:state.cash+action.payload}
+    case "GET_CASH":return {...state,cash:state.cash-action.payload}
+    default:
+      return state
+  }
+}
+const store = createStore(reducer);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+      </Provider>
   </React.StrictMode>
 );
 
